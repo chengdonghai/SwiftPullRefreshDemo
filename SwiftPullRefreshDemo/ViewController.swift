f6f8c1d544d494fb8bf0eabc72b5b7233c3f60ca
chengdonghai <dong723232@gmail.com>
2014-10-22 14:15:56 +0800

    Initial Commit

---
+++ SwiftPullRefreshDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPullRefreshDemo
+//
+//  Created by donghai cheng on 14-10-22.
+//  Copyright (c) 2014年 chd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
